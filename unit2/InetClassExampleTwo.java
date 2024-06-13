6b17e206b8d7a1143ce3cffb7eec17b7fae87120
Leah Deshar <leah.desar@gmail.com>
2024-06-13 21:41:11 +0545

    InetAddress

--- unit2/InetClassExampleTwo.java
+++ unit2/InetClassExampleTwo.java
@@ -11,44 +11,31 @@ public class InetClassExampleTwo {
             InetAddress[] inetAddresses = InetAddress.getAllByName("facebook.com");
             InetAddress[] inetAddressesNTC = InetAddress.getAllByName("www.ntc.net.np");
 
-            for (InetAddress inetAddress : inetAddresses) {
-                // Print the IP address
-                String ipAddress = inetAddress.getHostAddress();
-                System.out.println("IP Address: " + ipAddress);
-
-                // Print the hostname
-                String hostName = inetAddress.getHostName();
-                System.out.println("Host Name: " + hostName);
-
-                // Check if it is IPv4 or IPv6
-                if (inetAddress instanceof java.net.Inet4Address) {
-                    System.out.println("This is an IPv4 address.");
-                } else if (inetAddress instanceof java.net.Inet6Address) {
-                    System.out.println("This is an IPv6 address.");
-                }
-                System.out.println();
-            }
-
-            for (InetAddress ntc : inetAddressesNTC) {
-                // Print the IP address
-                String ipAddress = ntc.getHostAddress();
-                System.out.println("IP Address: " + ipAddress);
-
-                // Print the hostname
-                String hostName = ntc.getHostName();
-                System.out.println("Host Name: " + hostName);
-
-                // Check if it is IPv4 or IPv6
-                if (ntc instanceof java.net.Inet4Address) {
-                    System.out.println("This is an IPv4 address.");
-                } else if (ntc instanceof java.net.Inet6Address) {
-                    System.out.println("This is an IPv6 address.");
-                }
-                System.out.println();
-            }
+            Diplay(inetAddresses);
+            Diplay(inetAddressesNTC);
         } catch (UnknownHostException e) {
             System.err.println("Unable to resolve host");
             e.printStackTrace();
+        }
+    }
+
+    private static void Diplay(InetAddress[] inetAddressesNTC) {
+        for (InetAddress ntc : inetAddressesNTC) {
+            // Print the IP address
+            String ipAddress = ntc.getHostAddress();
+            System.out.println("IP Address: " + ipAddress);
+
+            // Print the hostname
+            String hostName = ntc.getHostName();
+            System.out.println("Host Name: " + hostName);
+
+            // Check if it is IPv4 or IPv6
+            if (ntc instanceof java.net.Inet4Address) {
+                System.out.println("This is an IPv4 address.");
+            } else if (ntc instanceof java.net.Inet6Address) {
+                System.out.println("This is an IPv6 address.");
+            }
+            System.out.println();
         }
     }
 }
