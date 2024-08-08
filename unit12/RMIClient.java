0558673cfa1b23c856a30ddfa5fedc32b3de74ee
Leah Deshar <leah.desar@gmail.com>
2024-08-08 18:12:17 +0545

    unit-12

---
+++ unit12/RMIClient.java
@@ -0,0 +1,25 @@
+package unit12;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Locale;
+public class RMIClient {
+    public static void main(String[] args) {
+        try {
+            Rem_Impt remObj = new Rem_Impt();
+            Registry retry = LocateRegistry.getRegistry(1888);
+            retry.rebind("Rem", retry);
+
+
+            int capture = remObj.Sum(5,5);
+            System.out.println("Sum :" + capture);
+
+            int factCapture = remObj.Factorial(5);
+            System.out.println("Factorial :" + factCapture);
+
+        }catch (RemoteException re){
+            System.out.println(re);
+        }
+
+    }
+}
